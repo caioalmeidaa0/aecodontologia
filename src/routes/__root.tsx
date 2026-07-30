c445199896ca73d6be27ca6b348ec51bb8e3e215
caioalmeidaa0 <caioalmeidaa0@gmail.com>
2026-07-29 21:35:43 -0300

    Update __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -91,6 +91,33 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
         href: appCss,
       },
     ],
+    scripts: [
+      {
+        src: "https://www.googletagmanager.com/gtag/js?id=AW-10857266319",
+        async: true,
+      },
+      {
+        children: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'AW-10857266319');
+
+          function gtag_report_conversion(url) {
+            var callback = function () {
+              if (typeof(url) != 'undefined') {
+                window.location = url;
+              }
+            };
+            gtag('event', 'conversion', {
+              'send_to': 'AW-10857266319/DH1VCNjK7tgcEI_5krko',
+              'event_callback': callback
+            });
+            return false;
+          }
+        `,
+      },
+    ],
   }),
   shellComponent: RootShell,
   component: RootComponent,
